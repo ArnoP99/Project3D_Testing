7a9689677438b5473fdcd498bfb3583183566311
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-10-04 10:08:57 +0200

    added HUD for players with bounds system

---
+++ VR Multiplayer project/Assets/boundstrigger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class boundstrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("Canvas/canvasBLACK").GetComponent<Image>().color = new Color(0, 0, 0, 255);
+    }
+
+}
