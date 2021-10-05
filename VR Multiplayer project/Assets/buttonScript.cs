2d952cd7db8dfe2636519b6f286d8e7166b59799
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-10-05 14:57:51 +0200

    Raycaster test

---
+++ VR Multiplayer project/Assets/buttonScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void DoClick()
+    {
+        Debug.Log("clicked");
+    }
+}
