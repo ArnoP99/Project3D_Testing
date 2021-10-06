ee69a3b3eca2658bcc574465a944289db586d273
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-10-06 14:29:04 +0200

    Trigger Script

---
+++ VR Multiplayer project/Assets/TriggerEvents.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvents : MonoBehaviour
+{
+
+    [SerializeField] Material defaultMaterial;
+    [SerializeField] Material highlightMaterial;
+
+    [SerializeField] GameObject gameObject;
+
+
+
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "LeftController" || collision.gameObject.tag == "RightController")
+        {
+            Debug.Log("Tag: " + collision.gameObject.tag);
+            gameObject.GetComponent<MeshRenderer>().material = highlightMaterial;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "LeftController" || collision.gameObject.tag == "RightController")
+        {
+            gameObject.GetComponent<MeshRenderer>().material = defaultMaterial;
+        }
+    }
+}
