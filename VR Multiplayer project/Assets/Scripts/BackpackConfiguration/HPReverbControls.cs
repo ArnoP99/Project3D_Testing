63bc5695cbd33360f5506ed0bea413def38b978a
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-10-22 15:22:57 +0200

    text system test

--- VR Multiplayer project/Assets/Scripts/BackpackConfiguration/HPReverbControls.cs
+++ VR Multiplayer project/Assets/Scripts/BackpackConfiguration/HPReverbControls.cs
@@ -14,34 +14,31 @@ public class HPReverbControls : MonoBehaviour
     {
         nurse = GameObject.FindGameObjectWithTag("Nurse");
         textPopUp = nurse.transform.GetChild(0).transform.GetChild(3).gameObject;
-        Debug.Log("Optie1: " + textPopUp.transform.GetChild(0).tag);
-        Debug.Log("Optie2: " + textPopUp.transform.GetChild(1).tag);
-        Debug.Log("Optie3: " + textPopUp.transform.GetChild(2).tag);
 
-        //if (textPopUp.transform.GetChild(0).GetComponent<TextMeshPro>().color == Color.red)
-        //{
-        //    activeChoice = textPopUp.transform.GetChild(0).gameObject;
-        //    Debug.Log("We got in the if");
-        //    textPopUp.SetActive(false);
-        //}
-        //else if (textPopUp.transform.GetChild(1).GetComponent<TextMeshPro>().color == Color.red)
-        //{
-        //    activeChoice = textPopUp.transform.GetChild(1).gameObject;
-        //    Debug.Log("We got in the if");
-        //    textPopUp.SetActive(false);
-        //}
-        //else if (textPopUp.transform.GetChild(2).GetComponent<TextMeshPro>().color == Color.red)
-        //{
-        //    activeChoice = textPopUp.transform.GetChild(2).gameObject;
-        //    Debug.Log("We got in the if");
-        //    textPopUp.SetActive(false);
-        //}
-        //else
-        //{
-        //    Debug.Log("No Active Choice Found.");
-        //}
+        if (textPopUp.transform.GetChild(0).GetComponent<TextMeshPro>().color == Color.red)
+        {
+            activeChoice = textPopUp.transform.GetChild(0).gameObject;
+            Debug.Log("We got in the if");
+            textPopUp.SetActive(false);
+        }
+        else if (textPopUp.transform.GetChild(1).GetComponent<TextMeshPro>().color == Color.red)
+        {
+            activeChoice = textPopUp.transform.GetChild(1).gameObject;
+            Debug.Log("We got in the if");
+            textPopUp.SetActive(false);
+        }
+        else if (textPopUp.transform.GetChild(2).GetComponent<TextMeshPro>().color == Color.red)
+        {
+            activeChoice = textPopUp.transform.GetChild(2).gameObject;
+            Debug.Log("We got in the if");
+            textPopUp.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("No Active Choice Found.");
+        }
 
-        //Debug.Log(activeChoice);
+        Debug.Log(activeChoice);
     }
 
     public void Joystick(InputAction.CallbackContext context)
