8c7ecdd547ec805604a19684cc5d4feb7571cae1
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-10-22 14:47:24 +0200

    debugging

--- VR Multiplayer project/Assets/Scripts/BackpackConfiguration/PlayerConfiguration.cs
+++ VR Multiplayer project/Assets/Scripts/BackpackConfiguration/PlayerConfiguration.cs
@@ -78,7 +78,6 @@ public class PlayerConfiguration : NetworkBehaviour
         if(PlayerID == 0)
         {
             this.GetComponentInChildren<MeshRenderer>().enabled = false;
-            this.transform.GetChild(0).transform.GetChild(3).gameObject.tag = "Server";
         }
         this.transform.parent = GameObject.Find("Players").transform; //Set 'Players" gameobject as parent
 
