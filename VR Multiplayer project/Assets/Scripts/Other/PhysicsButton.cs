7dd798535d6b497a89d5bc46e821eb846e662956
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-10-27 13:51:27 +0200

    Give player authority when executing command

--- VR Multiplayer project/Assets/Scripts/Other/PhysicsButton.cs
+++ VR Multiplayer project/Assets/Scripts/Other/PhysicsButton.cs
@@ -37,9 +37,9 @@ public class PhysicsButton : NetworkBehaviour
             GameObject visualRep = collision.gameObject.transform.parent.transform.parent.Find("VisualRepresentation").gameObject;
             GameObject player = collision.gameObject.transform.parent.transform.parent.transform.parent.gameObject;
 
-            //gameObject.GetComponent<NetworkIdentity>().AssignClientAuthority(this.GetComponent<NetworkIdentity>().connectionToClient);
+            player.GetComponent<NetworkIdentity>().AssignClientAuthority(this.GetComponent<NetworkIdentity>().connectionToClient);
             CmdMessageTest(player);
-            //gameObject.GetComponent<NetworkIdentity>().RemoveClientAuthority();
+            player.GetComponent<NetworkIdentity>().RemoveClientAuthority();
 
             if (gameObject.tag == "AgressorButton")
             {
