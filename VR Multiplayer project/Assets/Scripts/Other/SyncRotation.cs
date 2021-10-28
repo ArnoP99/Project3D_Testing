d8bd8789a90b6edb69ae6e8f301df3c10dafdeb9
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-10-28 14:28:16 +0200

    smooth rotation test

--- VR Multiplayer project/Assets/Scripts/Other/SyncRotation.cs
+++ VR Multiplayer project/Assets/Scripts/Other/SyncRotation.cs
@@ -1,34 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
+using System;
 
-public class SyncRotation : MonoBehaviour
+public class SyncRotation : NetworkBehaviour
 {
     GameObject playerCamera;
     GameObject visualRepresentation;
     GameObject textPlayer;
     Vector3 rot;
-
+    private Vector3 velocity = Vector3.zero;
+    public float smoothTime = 0.3F;
 
     private void Start()
     {
-        playerCamera = gameObject.transform.GetChild(0).transform.GetChild(0).gameObject;
-        visualRepresentation = gameObject.transform.GetChild(0).transform.GetChild(2).gameObject;
-        textPlayer = gameObject.transform.GetChild(0).transform.GetChild(3).gameObject;
+        if (isLocalPlayer)
+        {
+            playerCamera = gameObject.transform.GetChild(0).transform.GetChild(0).gameObject;
+            visualRepresentation = gameObject.transform.GetChild(0).transform.GetChild(2).gameObject;
+            textPlayer = gameObject.transform.GetChild(0).transform.GetChild(3).gameObject;
+        }
     }
 
 
     void Update()
     {
-        rot = new Vector3(0, playerCamera.transform.eulerAngles.y, 0);
-        visualRepresentation.transform.eulerAngles = rot;
-        if (playerCamera.transform.eulerAngles.y - textPlayer.transform.eulerAngles.y < -110 || playerCamera.transform.eulerAngles.y - textPlayer.transform.eulerAngles.y > 110)
+        if (isLocalPlayer)
         {
-            textPlayer.transform.eulerAngles = rot;
+            rot = new Vector3(0, playerCamera.transform.eulerAngles.y, 0);
+            visualRepresentation.transform.eulerAngles = rot;
+            if (playerCamera.transform.eulerAngles.y - textPlayer.transform.eulerAngles.y < -110 || playerCamera.transform.eulerAngles.y - textPlayer.transform.eulerAngles.y > 110)
+            {
+                textPlayer.transform.eulerAngles = Vector3.SmoothDamp(textPlayer.transform.position, rot, ref velocity, smoothTime);
+            }
         }
-
-
-
-        
     }
 }
