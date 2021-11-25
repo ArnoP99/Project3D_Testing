3a7cef3296eefb8d8e0ec44cdf61b1db003bf7ec
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-11-25 14:31:07 +0100

    Debugging TextSelection 4

--- VR Multiplayer project/Assets/Scripts/BackpackConfiguration/HPReverbControls.cs
+++ VR Multiplayer project/Assets/Scripts/BackpackConfiguration/HPReverbControls.cs
@@ -65,7 +65,7 @@ public class HPReverbControls : MonoBehaviour
     public void GetNurseActiveChoice()
     {
         nurse = GameObject.FindGameObjectWithTag("Nurse");
-        textPopUp = nurse.transform.parent.transform.parent.transform.GetChild(3).gameObject;
+        textPopUp = nurse.transform.parent.transform.GetChild(3).gameObject;
 
         if (textPopUp.transform.GetChild(0).GetComponent<TextMeshPro>().color == Color.red)
         {
@@ -93,7 +93,7 @@ public class HPReverbControls : MonoBehaviour
     public void GetAgressorActiveChoice()
     {
         agressor = GameObject.FindGameObjectWithTag("Agressor");
-        textPopUp = agressor.transform.parent.transform.parent.transform.GetChild(3).gameObject;
+        textPopUp = agressor.transform.parent.transform.GetChild(3).gameObject;
 
         if (textPopUp.transform.GetChild(0).GetComponent<TextMeshPro>().color == Color.red)
         {
