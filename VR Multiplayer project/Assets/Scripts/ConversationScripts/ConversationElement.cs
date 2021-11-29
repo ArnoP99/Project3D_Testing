9a921a4d6c903fb5032e306291d22f3aa9dacc83
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-11-29 16:24:35 +0100

    Debugging 15

--- VR Multiplayer project/Assets/Scripts/ConversationScripts/ConversationElement.cs
+++ VR Multiplayer project/Assets/Scripts/ConversationScripts/ConversationElement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConversationElement : MonoBehaviour
+public class ConversationElement
 {
     public enum ElementState
     {
