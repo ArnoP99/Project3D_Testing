2352e935c9b9bc254c839b3606609560293e9e3a
gilles1410 <33722252+gilles1410@users.noreply.github.com>
2021-12-01 15:21:16 +0100

    Debugging 12

--- VR Multiplayer project/Assets/Scripts/BackpackConfiguration/HPReverbControls.cs
+++ VR Multiplayer project/Assets/Scripts/BackpackConfiguration/HPReverbControls.cs
@@ -111,8 +111,6 @@ public class HPReverbControls : NetworkBehaviour
             if (gameObject.GetComponent<NetworkIdentity>().isClient == true && GameObject.Find("ConversationManager").GetComponent<ConversationManager>().ActiveConversation == -1)
             {
                 CmdSetConversation(1);
-                conversationManager = GameObject.Find("ConversationManager").gameObject.GetComponent<ConversationManager>();
-                activeReactionElements = conversationManager.GetActiveConversation().activeElement.ReactionElements;
             }
             if (gameObject.GetComponent<NetworkIdentity>().isClient == true && activeReactionElements.Count > 0)
             {
@@ -121,6 +119,8 @@ public class HPReverbControls : NetworkBehaviour
             }
             else
             {
+                conversationManager = GameObject.Find("ConversationManager").gameObject.GetComponent<ConversationManager>();
+                activeReactionElements = conversationManager.GetActiveConversation().activeElement.ReactionElements;
                 CmdUpdateAgressorText();
             }
         }
